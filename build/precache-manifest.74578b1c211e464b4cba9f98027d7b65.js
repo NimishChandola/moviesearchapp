78dc9c81cc1183b5507245050fffc2c64a3160c5
Nimish Chandola <nimish@Nimishs-MacBook-Pro.local>
2020-09-08 22:34:07 +0530

    added prod build folder

---
+++ build/precache-manifest.74578b1c211e464b4cba9f98027d7b65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72311f32116c1c8453ce97c87514b91e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "601582ca924d265a03c9",
+    "url": "/static/css/main.37dcd872.chunk.css"
+  },
+  {
+    "revision": "6ad74a55319ab811aace",
+    "url": "/static/js/2.10c0ab2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10c0ab2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601582ca924d265a03c9",
+    "url": "/static/js/main.1008b301.chunk.js"
+  },
+  {
+    "revision": "f8fb1890f5de3a071fc6",
+    "url": "/static/js/runtime-main.07e574ad.js"
+  }
+]);
